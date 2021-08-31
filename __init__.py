bc2d248accd04d6076622c898eb3bac2b51bfbe1
wry <1790454752@qq.com>
2021-08-31 19:51:13 +0800

    solve the problem of not finding mimes.json

--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
 """
+#   Table of Content
+
+- [Name](#name)
+- [Status](#status)
+- [Synopsis](#synopsis)
+- [Author](#author)
+- [Copyright and License](#copyright-and-license)
+
+
 #   Name
 
 k3mime
@@ -10,7 +19,7 @@ The library is considered production ready.
 """
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __name__ = "k3mime"
 
 from .mime import (
